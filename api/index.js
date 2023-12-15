8f69c78d8a0162f630b0a5c9f8a32be5b367267f
iggy4321 <i.george.21@abdn.ac.uk>
2023-12-15 04:58:44 +0000

    create the middleware function to handle possible errors

--- api/index.js
+++ api/index.js
@@ -21,6 +21,24 @@ app.use(express.json()); // Body parser for JSON payloads
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 
+
+
+app.use((err, req, res, next) => {
+
+    const statusCode = err.statusCode || 500;
+    const message = err.message || "Internal Server Error";
+    return res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message,
+    });
+
+});
+
+
+
+
+
 app.use(express.json());
 
 
