29ae8cf37f9ef285b3adcfd504e33a39b8921119
iggy4321 <i.george.21@abdn.ac.uk>
2023-12-15 04:12:41 +0000

    test api route

--- api/index.js
+++ api/index.js
@@ -1,17 +1,29 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRouter from "./routes/user.routes.js";
 
 dotenv.config();
 
 mongoose.connect(process.env.MONGO).then(() => {
-    console.log('Connected to MongoDB')
+    console.log('Connected to MongoDB');
 }).catch((err) => {
-    console.log(err)
+    console.error('Failed to connect to MongoDB', err);
+    process.exit(1);
 });
 
 const app = express();
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000')
+app.use(express.json()); // Body parser for JSON payloads
+
+app.use("/api/user", userRouter);
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`Server is running on port ${process.env.PORT || 3000}`);
+});
+
+// General error handler (optional, can be more sophisticated)
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
 });
