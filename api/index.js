4cff30325b61e00bedfebfc025050287ff9c6b9f
iggy4321 <i.george.21@abdn.ac.uk>
2023-12-15 04:49:46 +0000

    create signup

--- api/index.js
+++ api/index.js
@@ -2,6 +2,8 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from "./routes/user.routes.js";
+import authRouter from "./routes/auth.route.js";
+import exp from 'constants';
 
 dotenv.config();
 
@@ -17,6 +19,10 @@ const app = express();
 app.use(express.json()); // Body parser for JSON payloads
 
 app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
+
+app.use(express.json());
+
 
 app.listen(process.env.PORT || 3000, () => {
     console.log(`Server is running on port ${process.env.PORT || 3000}`);
