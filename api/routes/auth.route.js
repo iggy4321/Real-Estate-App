2ee319593e45dbec2af7a65217669f3ae8060b67
iggy4321 <i.george.21@abdn.ac.uk>
2023-12-27 23:17:54 +0000

    complete signout

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { google, signin, signup } from '../controller/auth.controller.js';
+import { google, signin, signup, signOut } from '../controller/auth.controller.js';
 
 const router = express.Router();
 
@@ -7,5 +7,6 @@ const router = express.Router();
 router.post('/signup', signup)
 router.post('/signin', signin)
 router.post('/google', google)
+router.get('/signout', signOut)
 
 export default router;
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -14,6 +14,9 @@ import {
   deleteUserFailure,
   deleteUserStart,
   deleteUserSuccess,
+  signOutUserStart,
+  signOutUserFailure,
+  signOutUserSuccess,
 } from '../redux/user/userSlice';
 import { useDispatch } from 'react-redux';
 
@@ -101,6 +104,22 @@ export default function Profile() {
     }
   };
 
+  const handleSignOut = async() => {
+    try{
+      dispatch(signOutUserStart());
+      const res = await fetch ('/api/auth/signout');
+      const data = await res.json();
+      if (data === false) {
+        dispatch(signOutUserFailure(data.message));
+        return;
+      }
+      dispatch(signOutUserSuccess(data));
+    } catch (error){
+      dispatch(signOutUserFailure(data.message))
+    }
+  }
+
+
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
@@ -168,7 +187,7 @@ export default function Profile() {
         >
           Delete account
         </span>
-        <span className='text-red-700 cursor-pointer'>Sign out</span>
+        <span onClick={handleSignOut} className='text-red-700 cursor-pointer'>Sign out</span>
       </div>
 
       <p className='text-red-700 mt-5'>{error ? error : ''}</p>
